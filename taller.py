32c174d834182bc12caf7af5217e04d17e0ea551
juan <juanpajaro@javeriana.edu.co>
2025-09-06 14:20:26 -0500

    ajustando el taller.py

--- taller.py
+++ taller.py
@@ -30,3 +30,118 @@ print("Size of vocabulary: ", V)
 # example of word to index mapping
 print("Index of the word 'dulcinea' :  ",word2Ind['dulcinea'])
 print("Word which has index 8097:  ",Ind2word[8096])
+
+def initialize_model(N,V, random_seed=1):
+    '''
+    Inputs:
+        N:  dimension of hidden vector
+        V:  dimension of vocabulary
+        random_seed: random seed for consistent results in the unit tests
+     Outputs:
+        W1, W2, b1, b2: initialized weights and biases
+    '''
+
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+    np.random.seed(random_seed)
+    # W1 has shape (N,V)
+    W1 = np.random.rand(N, V)
+
+    # W2 has shape (V,N)
+    W2 = np.random.rand(V, N)
+
+    # b1 has shape (N,1)
+    b1 = np.random.rand(N, 1)
+
+    # b2 has shape (V,1)
+    b2 = np.random.rand(V, 1)
+
+    ### END CODE HERE ###
+    return W1, W2, b1, b2
+
+tmp_N = 4
+tmp_V = 10
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(tmp_N,tmp_V)
+assert tmp_W1.shape == ((tmp_N,tmp_V))
+assert tmp_W2.shape == ((tmp_V,tmp_N))
+print(f"tmp_W1.shape: {tmp_W1.shape}")
+print(f"tmp_W2.shape: {tmp_W2.shape}")
+print(f"tmp_b1.shape: {tmp_b1.shape}")
+print(f"tmp_b2.shape: {tmp_b2.shape}")
+
+def softmax(z):
+    '''
+    Inputs:
+        z: output scores from the hidden layer
+    Outputs:
+        yhat: prediction (estimate of y)
+    '''
+    ### START CODE HERE (Replace instances of 'None' with your own code) ###
+    # Calculate yhat (softmax)
+    yhat = np.exp(z)/ np.sum(np.exp(z), axis = 0)
+    ### END CODE HERE ###
+    return yhat
+
+# Test the function
+tmp = np.array([[1,2,3],
+                [1,1,1]
+               ])
+tmp_sm = softmax(tmp)
+print(tmp_sm)
+
+def forward_prop(x, W1, W2, b1, b2):
+    '''
+    Inputs:
+        x:  average one hot vector for the context
+        W1, W2, b1, b2:  matrices and biases to be learned
+     Outputs:
+        z:  output score vector
+    '''
+
+    ### START CODE HERE (Replace instances of 'None' with your own code) ###
+    # Calculate h
+    h = np.dot(W1, x) + b1
+
+    # Apply the relu on h,
+    # store the relu in h
+    h = np.maximum(0, h)
+
+    # Calculate z
+    z = np.dot(W2, h) + b2
+
+    ### END CODE HERE ###
+
+    return z, h
+
+# Create some inputs
+tmp_N = 2
+tmp_V = 3
+tmp_x = np.array([[0,1,0]]).T
+#print(tmp_x)
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(N=tmp_N,V=tmp_V, random_seed=1)
+
+print(f"x has shape {tmp_x.shape}")
+print(f"N is {tmp_N} and vocabulary size V is {tmp_V}")
+
+# call function
+tmp_z, tmp_h = forward_prop(tmp_x, tmp_W1, tmp_W2, tmp_b1, tmp_b2)
+print("call forward_prop")
+print()
+# Look at output
+print(f"z has shape {tmp_z.shape}")
+print("z has values:")
+print(tmp_z)
+
+print()
+
+print(f"h has shape {tmp_h.shape}")
+print("h has values:")
+print(tmp_h)
+
+# compute_cost: cross-entropy cost function
+def compute_cost(y, yhat, batch_size):
+
+    # cost function
+    logprobs = np.multiply(np.log(yhat),y)  + np.multiply(np.log(1 - yhat), 1 - y)
+    cost = - 1/batch_size * np.sum(logprobs)
+    cost = np.squeeze(cost)
+    return cost
